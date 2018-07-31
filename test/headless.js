cfbb2be7ab5931dc809fe52242724178fddabf74
MirEnovi <garciaivonne11@gmail.com>
2018-07-31 17:39:56 -0500

    Agrega ideas principales sobre test unitarios

--- test/headless.js
+++ test/headless.js
@@ -2,5 +2,6 @@
 global.window = global;
 global.assert = require('chai').assert;
 require('../src/data');
+require('../src/drawnProfiles.js');
+require('../src/posts.js');
 require('./data.spec.js');
-
